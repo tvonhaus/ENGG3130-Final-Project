7276eca597e4ac33f4faaf8ff5f848e9cc07f8a4
Taylor von Hausen <tvonhausen@gmail.com>
2020-03-23 21:15:32 -0700

    Added figures dir. Added variations of logistic map eqn

--- logistic_map.py
+++ logistic_map.py
@@ -1,46 +1,59 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 def logistic_eqn(rate,x):
     return rate * x * (1 - x)
 
-# # Creates 50 (default) evenly spaced numbers between interval of 0 to 1
-# x =  np.linspace(0,1)
-# # Creates y values for logistic eqn with rate of 2 and x values between 0 and 1
-# y = logistic_eqn(2,x)
+def logistic_eqn_2(rate,x):
+    return rate * x * (1 - x**2)
 
-# # Creates a figure containing a single axes
-# fig, ax = plt.subplots()
-# ax.plot(x,y)
-# ax.set_xlabel('xo')
-# ax.set_ylabel('xn')
-# ax.set_title('r = 2')
-# plt.show()
+def logistic_eqn_3(rate,x):
+    return rate * x * (1 - x**3)
 
-# y = logistic_eqn(3,x)
+def logistic_eqn_4(rate,x):
+    return rate * x * ((1 - x)**2)
 
-# fig, ax = plt.subplots()
-# ax.plot(x,y)
-# ax.set_xlabel('xo')
-# ax.set_ylabel('xn')
-# ax.set_title('r=3')
-# plt.show()
+def logistic_eqn_5(rate,x):
+    return rate * x * ((1 - x)**3)
 
-n = 10000
+
+# Creates 50 (default) evenly spaced numbers between interval of 0 to 1
+x =  np.linspace(0,1)
+# Creates y values for logistic eqn with rate of 2 and x values between 0 and 1
+y = logistic_eqn_5(2,x)
+
+# Creates a figure containing a single axes
+fig, ax = plt.subplots()
+ax.plot(x,y)
+ax.set_xlabel('xo')
+ax.set_ylabel('xn')
+ax.set_title('r = 2')
+plt.show()
+# Creates y values for logistic eqn with rate of 3 and x values between 0 and 1
+y = logistic_eqn_5(3,x)
+
+fig, ax = plt.subplots()
+ax.plot(x,y)
+ax.set_xlabel('xo')
+ax.set_ylabel('xn')
+ax.set_title('r=3')
+plt.show()
+
+# Creates an array n rate values between
+n = 1000
 r = np.linspace(0,4,n)
-iterations = 1000
+iterations = 500
 # x = 0.5 * np.ones(n)
 x = 0.5
 print(x)
 
 fig, ax1 = plt.subplots()
 for i in range(iterations):
-    x = logistic_eqn(r,x)
+    x = logistic_eqn_5(r,x)
     ax1.plot(r,x)
 ax1.set_title('Bifurcation Diagram')
 ax1.set_xlabel('Rate')
 ax1.set_ylabel('Equilibrium Population')
 ax1.legend()
 plt.show()
-
 
